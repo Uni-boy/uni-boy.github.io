ec80b6cd30abf043562347d34eae2a52eed6dae6
Uni-boy <liu.xinzh@northeastern.edu>
2024-02-23 23:36:33 -0500

    full page

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "images.unsplash.com",
+    //   },
+    // ],
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: true,
+  },
+  output: 'export',
+};
+
+module.exports = nextConfig;
